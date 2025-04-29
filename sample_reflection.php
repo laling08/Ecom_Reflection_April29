37957059e5e1685ff7a6585fced435aa448c1c66
Ishilia <ishiliagilcedeslabrador@gmail.com>
2025-04-29 10:30:33 -0400

    added a get class name and get method name code block

--- sample_reflection.php
+++ sample_reflection.php
@@ -5,13 +5,26 @@ class SampleClass {
     private $property2 = "value2";
     private $property3 = "value2";
 
-    public funtion method1 ($param) {
+    public function method1 ($param) {
         return "method1 param: " . $param;
     }
 
-    public funtion method2 ($param) {
+    public function method2 ($param) {
         return "method2 param: " . $param;
     }
+}
+
+// Create a ReflectionClass instance
+$reflector = new ReflectionClass('SampleClass');
+
+// Get class name
+echo "Class Name;". $reflector->getName() . "<br />";
+
+// Get class methods
+echo "Methods: " . "<br />";
+$methods = $reflector->getMethods();
+foreach ($methods as $method) {
+    echo "- " . $method->getName() . "<br />";
 }
 
 ?>
