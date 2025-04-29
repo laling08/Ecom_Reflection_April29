d16905b5e5adcfa62959e843b4a0fc29f7391de3
Ishilia <ishiliagilcedeslabrador@gmail.com>
2025-04-29 10:21:26 -0400

    added dummy methods

--- sample_reflection.php
+++ sample_reflection.php
@@ -4,6 +4,14 @@ class SampleClass {
     private $property1 = "value1";
     private $property2 = "value2";
     private $property3 = "value2";
-    
+
+    public funtion method1 ($param) {
+        return "method1 param: " . $param;
+    }
+
+    public funtion method2 ($param) {
+        return "method2 param: " . $param;
+    }
 }
+
 ?>
